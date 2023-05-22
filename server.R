d7084bce61d345458566afabfb495537f0211985
Devanshi Desai <devanshidesai@Devanshis-MacBook-Air.local>
2023-05-22 10:03:20 -0700

    completed shiny app visualization

--- server.R
+++ server.R
@@ -5,19 +5,72 @@ library("plotly")
 
 climate_change_data <- read.csv("~/Desktop/climate_change_data.csv")
 
-# Server
-my_server <- function(input, output) {
-  
-  output$oil_emissions <- renderPlotly({
-    
-    selected_countries_data <- climate_change_data %>% filter(country %in% input$user_selection)
+##SUMMARY VARIABLES
+#include information only about india, china, and united states
+selected_countries_data <- climate_change_data %>%
+  filter(country %in% c("United States", "China", "India"))
 
-    oil_emissions <- ggplot(selected_countries_data, aes(x = year, y = oil_co2, color = country)
-                            + geom_line()
-                            + labs(title = "Oil-based Carbon Emissions by Country",
-                                 x = "Year",
-                                 y = "Emissions")
+co2_consumption_by_country <- selected_countries_data %>%
+  group_by(country) %>%
+  summarise(consumption_co2 = sum(consumption_co2, na.rm=TRUE))
+
+#variable 1: highest consumption based co2 emissions
+highest_consumption_co2 <- co2_consumption_by_country %>%
+  filter(consumption_co2 == max(consumption_co2)) %>%
+  pull(consumption_co2)
+
+#variable 2: country with highest consumption based co2 emissions
+highest_consumption_country <- co2_consumption_by_country %>%
+  filter(consumption_co2 == max(consumption_co2)) %>%
+  pull(country)
+
+#variable 3: the year highest percentage increase in co2 emissions in US 
+highest_co2_growth_year <- selected_countries_data %>%
+  filter(country == "United States") %>%
+  filter(co2_growth_prct %in% max(co2_growth_prct, na.rm = TRUE)) %>%
+  pull(year)
+
+#extra info - what was the percentage increase in co2 emissions
+highest_co2_growth_prct_us <- selected_countries_data %>%
+  filter(country == "United States") %>%
+  filter(co2_growth_prct %in% max(co2_growth_prct, na.rm = TRUE)) %>%
+  pull(co2_growth_prct)
+
+#variable 4: cumulative oil based co2 emissions in china
+china_cumulative_oil_co2 <- selected_countries_data %>%
+  filter(country == "China") %>%
+  filter(year == max(year)) %>%
+  pull(cumulative_oil_co2)
+
+#variable 5: cumulative coal based co2 emissions in india
+india_cumulative_coal_co2 <- selected_countries_data %>%
+  filter(country == "India") %>%
+  filter(year == max(year)) %>%
+  pull(cumulative_coal_co2)
+
+#variable 6: cumulative gas based co2 emissions in the us 
+us_cumulative_gas_co2 <- selected_countries_data %>%
+  filter(country == "United States") %>%
+  filter(year == max(year)) %>%
+  pull(cumulative_oil_co2)
+
+# Server
+my_server <- function(input, output, session) {
+  
+  output$emissions <- renderPlotly({
     
-    return(ggplotly(oil_emissions,  tooltip = "text"))
+    selected_countries_years <- climate_change_data %>% 
+      filter(country %in% input$user_selection) %>%
+      filter(year >= input$year_range[1], year <= input$year_range[2])
+  
+    oil_emissions <- ggplot(selected_countries_years, aes(x = year, y = oil_co2, color = country)) + 
+      geom_line() +
+      labs(title = "Oil-based Carbon Emissions by Country",
+           x = "Year",
+           y = "Emissions")
+    
+    return(ggplotly(oil_emissions, tooltip = "text"))
+    
   })
+  
 }
--- ui.R
+++ ui.R
@@ -1,23 +1,39 @@
+library("dplyr")
+library("ggplot2")
+library("plotly")
 # Define UI for application
 
 # Load data
 climate_change_data <- read.csv("~/Desktop/climate_change_data.csv")
 
 # User Interface
-my_ui <- fluidPage(
+my_ui <-  navbarPage(
+  title = "Assignment 4: Climate Change, Devanshi Desai",
+  id = "navbar",
   
-  h1("Assignment 4: Climate Change", align="center"),
-  h2("Devanshi Desai", align="center"),
+  tabPanel("Summary Information",
+    includeMarkdown("~/Desktop/info201/assignments/a4-climate-change-devanshidesai25/index.Rmd")    
+  ),
   
-  plotlyOutput(outputId = "oil_emissions"),
-  
-  selectInput(
+  tabPanel("Visualization",
+    plotlyOutput(outputId = "emissions"),
+           
+    selectInput(
     inputId = "user_selection",
     label = "Countries",
     choices = climate_change_data$country,
     selected = "United States",
     multiple = TRUE
+    ),
+    
+    sliderInput(
+      inputId = "year_range",
+      label = "Select Year Range:",
+      min = 1800,
+      max = 2021,
+      value = c(1800, 2021),
+      step = 1
+    )
   )
   
-
 )
