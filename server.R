be555c943226c4e60ef6333448f9e3813c049b1d
Devanshi Desai <devanshidesai@Devanshis-MacBook-Air.local>
2023-05-24 09:21:57 -0700

    manually entered variables from summary.R

--- server.R
+++ server.R
@@ -5,54 +5,15 @@ library("plotly")
 
 climate_change_data <- read.csv("climate_change_data.csv")
 
-##SUMMARY VARIABLES
-#include information only about india, china, and united states
-selected_countries_data <- climate_change_data %>%
-  filter(country %in% c("United States", "China", "India"))
+#variables from summary.r document
+china_cumulative_oil_co2 <- 34491.543
+highest_co2_growth_prct_us <- 33.495
+highest_co2_growth_year <- 1832
+highest_consumption_co2 <- 180878.538
+highest_consumption_country <- "United States"
+india_cumulative_coal_co2 <- 37018.082
+us_cumulative_gas_co2 <- 161275.016
 
-co2_consumption_by_country <- selected_countries_data %>%
-  group_by(country) %>%
-  summarise(consumption_co2 = sum(consumption_co2, na.rm=TRUE))
-
-#variable 1: highest consumption based co2 emissions
-highest_consumption_co2 <- co2_consumption_by_country %>%
-  filter(consumption_co2 == max(consumption_co2)) %>%
-  pull(consumption_co2)
-
-#variable 2: country with highest consumption based co2 emissions
-highest_consumption_country <- co2_consumption_by_country %>%
-  filter(consumption_co2 == max(consumption_co2)) %>%
-  pull(country)
-
-#variable 3: the year highest percentage increase in co2 emissions in US 
-highest_co2_growth_year <- selected_countries_data %>%
-  filter(country == "United States") %>%
-  filter(co2_growth_prct %in% max(co2_growth_prct, na.rm = TRUE)) %>%
-  pull(year)
-
-#extra info - what was the percentage increase in co2 emissions
-highest_co2_growth_prct_us <- selected_countries_data %>%
-  filter(country == "United States") %>%
-  filter(co2_growth_prct %in% max(co2_growth_prct, na.rm = TRUE)) %>%
-  pull(co2_growth_prct)
-
-#variable 4: cumulative oil based co2 emissions in china
-china_cumulative_oil_co2 <- selected_countries_data %>%
-  filter(country == "China") %>%
-  filter(year == max(year)) %>%
-  pull(cumulative_oil_co2)
-
-#variable 5: cumulative coal based co2 emissions in india
-india_cumulative_coal_co2 <- selected_countries_data %>%
-  filter(country == "India") %>%
-  filter(year == max(year)) %>%
-  pull(cumulative_coal_co2)
-
-#variable 6: cumulative gas based co2 emissions in the us 
-us_cumulative_gas_co2 <- selected_countries_data %>%
-  filter(country == "United States") %>%
-  filter(year == max(year)) %>%
-  pull(cumulative_oil_co2)
 
 # Server
 my_server <- function(input, output, session) {
